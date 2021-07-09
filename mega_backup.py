84f209e104442982bdd6ea8e3568aff14591dc32
ekiafa <kiafaeftychia@gmail.com>
2021-07-09 21:18:43 +0300

    bug with destination folder insertion

--- mega_backup.py
+++ mega_backup.py
@@ -1,5 +1,5 @@
 
-import sys,os,time
+import os
 from mega import Mega
 
 
@@ -8,21 +8,32 @@ global destination_directory
 global email
 global password
 global list_of_files
+path=None
+
 def upload(list_of_files,path):
   
     mega = Mega({'verbose': True})
     m = mega.login(email,password)
-  
+    #print(path)
     for src in list_of_files:
+      print(src)
       srcFile = current_directory + src
       if os.path.isfile( srcFile ):
 
         
-        destination_directory=m.find(path)
-        uppedFile = m.upload(srcFile, destination_directory)
+        #folder=m.find(path)
+        
+        
+        #uppedFile = m.upload(src,folder[0])
+        #else:
+        uppedFile = m.upload(src, None)
+        
+        
         
         print("Successful upload!")
         print ('Upped File Link: ' + m.get_upload_link( uppedFile ))
+      else:
+        print("There is no source file with this name.Please check your input.")
 
 
 
@@ -30,9 +41,11 @@ def main():
 
   global current_directory
   global destination_directory
-  current_directory = os.getcwd() + os.path.sep #et path name of the working directory
-  destination_directory = None
+  current_directory = os.getcwd() + os.path.sep #get path name of the working directory
+ 
   
+  
+
   if len(list_of_files)>=1 and path!=None:
     upload(list_of_files,path)
   
@@ -42,7 +55,7 @@ if __name__ == '__main__':
   print("------------------------------------------Upload file Mega.nz tool-------------------------------------------------")
   email=input("Please insert your Mega's account email: ")
   password=input("Please insert your Mega's account password :")
-  list_of_files=input("Please insert file names separated by spaces :")
+  list_of_files=input("Please insert file names separated by spaces :").split(" ")
   path=input("Please insert your destination path in Mega's Cloud Drive :")
   main()
   
---
+++ px.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from mega import Mega
+
+mega = Mega({'verbose': True})
+m = mega.login('eftihiakf@yahoo.gr', 'noinfneeded')
+
+curDir = os.getcwd() + os.path.sep
+dstDirId = None
+
+if len(sys.argv)>1 and sys.argv[1]:
+	srcs = sys.argv[1:]
+else:
+	srcs = None
+
+#print srcs
+
+if srcs:
+	if len(srcs) > 1 and not os.path.isfile( srcs[-1] ):#if last param isn't a local file then it is dest remote folder
+		dstName = srcs.pop()
+		dstId = m.find( dstName )
+		if dstId:
+			dstDirId = dstId[0]
+			print( 'Destination Folder: '+ dstName)
+
+	for src in srcs:
+		srcFile = curDir + src
+		if os.path.isfile( srcFile ):
+			#print srcFile
+			#print dstDirId
+			uppedFile = m.upload(srcFile, dstDirId)
+			#print uppedFile
+			print ('Upped File Link: ' + m.get_upload_link( uppedFile ))
