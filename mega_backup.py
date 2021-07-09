f7a25bb26a1eed6c27e0f45bc28445269ebaf7ed
ekiafa <kiafaeftychia@gmail.com>
2021-07-09 18:09:17 +0300

    messages for user inputs

--- mega_backup.py
+++ mega_backup.py
@@ -9,30 +9,30 @@ from mega import Mega
 
 global current_directory
 global destination_directory
-
+global email
+global password
+global list_of_files
 def upload(list_of_files,path):
   
     mega = Mega({'verbose': True})
-    m = mega.login('eftihiakf@yahoo.gr', 'noinfneeded')
+    m = mega.login(email,password)
   
     for src in list_of_files:
       srcFile = current_directory + src
       if os.path.isfile( srcFile ):
 
-        #print (srcFile)
+        
         destination_directory=m.find(path)
         #print (destination_directory)
         uppedFile = m.upload(srcFile, destination_directory)
-        print (uppedFile)
+        
+        print("Successful upload!")
         print ('Upped File Link: ' + m.get_upload_link( uppedFile ))
 
 
 
 def main():
 
-  
-  list_of_files=sys.argv[1:-1]
-  path=sys.argv[-1]
   global current_directory
   global destination_directory
   current_directory = os.getcwd() + os.path.sep #et path name of the working directory
@@ -44,6 +44,10 @@ def main():
   
 
 if __name__ == '__main__':
-  
+  print("------------------------------------------Upload file Mega.nz tool-------------------------------------------------")
+  email=input("Please insert your Mega's account email: ")
+  password=input("Please insert your Mega's account password :")
+  list_of_files=input("Please insert file names separated by spaces :")
+  path=input("Please insert your destination path in Mega's Cloud Drive :")
   main()
   
